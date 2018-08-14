bba64988d6e142d6d5b2f50be7ad7ba396507ca8
root <root@ip-10-0-0-159.us-east-2.compute.internal>
2018-08-14 13:32:28 +0000

    changed in AWs

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '192.168.99.100',
+    host     : 'localhost',
     user     : 'root',
     password : 'root',
     database : 'certificates'
